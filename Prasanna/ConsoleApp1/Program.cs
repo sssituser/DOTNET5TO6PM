8c1790d3e75349a868c9865b424bc0f986e6ba1a
Prasanna <korrapatiprasanna71@gmail.com>
2025-09-18 18:26:52 +0530

    files added

---
+++ Prasanna/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter end value");//end=1
+            int end = int.Parse (Console.ReadLine());
+            Console.WriteLine("enter start value");//start=20
+            int start = int.Parse(Console.ReadLine());
+            while (end <= start)
+            {
+                Console.WriteLine(start);
+                start-=2;
+            }
+        }
+    }
+}
