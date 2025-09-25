9a210ed19d89c7b878d2381547469172e5577cda
shannug98 <shanmukagaddam98@gmail.com>
2025-09-25 20:57:23 +0530

    25-09

---
+++ shannu/ArraysPrograms/Program6.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysPrograms
+{
+    internal class Program6
+    {
+        /* Write a program to read elements for rectangular array of size 3
+display the diagonal elements. */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A matrix of m*n");
+            int[,] A = new int[3, 3];
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    A[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            //Display MAtrix and Diagonal Elements
+            Console.WriteLine("A matrix");
+            for (int i=0;i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    
+                    Console.Write($"{A[i,j]}\t");
+                    
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Diagonal elements in matrix are : ");
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+
+                    if (i == j)
+                    {
+                        
+                        Console.WriteLine($"{A[i, j]}\t");
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ shannu/ArraysPrograms/Program7.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysPrograms
+{
+    internal class Program7
+    {
+        /*Write program to read elements for the array of size 2x3
+and display them find the transpose of the matrix.*/
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter num of rows : ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num of columns : ");
+            int columns = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Matrix of m*n");
+            int[,] A = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    A[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            //================================================
+            Console.WriteLine("Display the matrix m*n :");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{A[i,j]}\t");
+                }
+                Console.WriteLine();
+            }
+            //==============Transpose of MAtrix=====================
+            Console.WriteLine("Transpose of matrix : ");
+            for(int i = 0;i < columns; i++)
+            {
+                for(int j = 0;j < rows; j++)
+                {
+                    Console.Write($"{A[j,i]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
