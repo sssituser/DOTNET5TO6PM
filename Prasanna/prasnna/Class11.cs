7a948143c2aa373c6e122044351007b6f520b311
Prasanna <korrapatiprasanna71@gmail.com>
2025-10-09 16:17:00 +0530

    method examples added

---
+++ Prasanna/prasnna/Class11.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodsexamples
+{
+    internal class Class11
+    {
+            public static int GetMaxNum(int[] array)
+            {
+                return ArrayToNum(SortArrayInDesc(GetMaxDigitsArray(array)));
+            }
+            public static int[] GetMaxDigitsArray(int[] array) 
+            {
+                int[] res = new int[array.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    res[i] = GetMaxDigit(array[i]);
+                }
+                return res;
+
+            }
+
+            public static int GetMaxDigit(int num) 
+            {
+                int max = num % 10; 
+                while (num > 0)
+                {
+                    int digit = num % 10;
+                    if (max < digit) 
+                    {
+                        max = digit; 
+                    }
+                    num /= 10;
+
+                }
+                return max;
+            }
+            public static int[] SortArrayInDesc(int[] array) 
+            {
+                Array.Sort(array);
+                Array.Reverse(array);
+                return array;
+            }
+
+            public static int ArrayToNum(int[] array) 
+            {
+                int res = 0;
+                foreach (int i in array) 
+                {
+                    res = res * 10 + i; 
+                }
+                return res;
+            }
+            static void Main(string[] args)
+            {
+                int[] array = { 153, 244, 789, 678, 786, 987 };
+                Console.WriteLine(GetMaxNum(array));
+
+            }
+    }
+}
+
---
+++ Prasanna/prasnna/Class8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodsexamples
+{
+    internal class Class8
+    {
+        public static string StrongNumlist(int start, int end)
+        {
+            string res = string.Empty;
+            for (int i = start; i <= end; i++)
+            {
+                if (Class7.IsStrong(i))
+                {
+                    res = res + i + ",";
+                }
+            }
+            return res.Substring(0, res.Length - 1) + ".";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter start value");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter end value");
+            int end = int.Parse(Console.ReadLine());
+            Console.WriteLine($"List Of Strong Numbers {StrongNumlist(start, end)}");
+        }
+    }
+}
